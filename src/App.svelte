04d1b701370248ad89b3188072b793388bcc2049
Daniel Irvine <daniel@danielirvine.com>
2019-12-06 20:08:54 +0000

    Add styles and simplify API a little bit

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,16 @@
 <script>
-  import { emptyTodo, makeTodo, markAsDone } from './todo.js';
-  import { add, findMatching, update } from './todoList.js';
+  import { emptyTodo, markAsDone } from './todo.js';
+  import { add, findAllMatching, update } from './todoList.js';
 
-  let formValues = emptyTodo();
+  let currentTodo = emptyTodo();
   let filter = '';
-  let todos = findMatching(filter);
+  let todos = findAllMatching(filter);
 
   const handleAdd = e => {
     e.preventDefault();
     try {
-      add(makeTodo(formValues));
-      formValues = emptyTodo();
+      add(currentTodo);
+      currentTodo = emptyTodo();
       updateTodos();
     } catch (e) {
       alert(e.message);
@@ -18,45 +18,92 @@
   };
 
   const updateTodos = () => {
-    todos = findMatching(filter);
+    todos = findAllMatching(filter);
+  };
+
+  const markAsDoneAndUpdate = todo => {
+    update(markAsDone(todo));
+    updateTodos();
   };
 
 </script>
 
 <style>
-  p {
-    max-width: 90ch;
+  main {
+    max-width: 70ch;
     margin: 0 auto;
-    font-size: 1.4em;
     font-family: sans-serif;
   }
+
+  table {
+    border-spacing: 0;
+    table-layout: fixed;
+  }
+
+  thead {
+    background-color: #cde;
+  }
+
+  td {
+    padding: 5px;
+  }
+
+  input {
+    font-size: 1em;
+    border: 1px solid #abc;
+    margin: 0;
+    padding: 5px;
+    width: 100%;
+    box-sizing: border-box;
+  }
+
+  input[type="submit"], input[type="button"] {
+    padding: 5px;
+    background-color: #345;
+    color: white;
+    border-radius: 2px;
+    border: 0;
+    box-shadow: none;
+    width: 150px;
+  }
+
+  table {
+    width: 100%;
+  }
+
+  td:nth-child(2), td:nth-child(3) {
+    width: 150px;
+  }
+
 </style>
 
-<form on:submit={handleAdd}>
-  <input bind:value={formValues.title} id="title" placeholder="Title" />
-  <input bind:value={formValues.dueDate} type="date" id="date" placeholder="Due date" />
-  <input type="submit" value="Add" />
-</form>
+<main>
 
-<input bind:value={filter} on:change={updateTodos} id="filter" placeholder="Filter..." />
-
-<table>
-  {#each todos as todo}
-  <tr>
-    <td>
-      {#if todo.completedAt}
-      <p><s>{todo.title}</s></p>
-      {:else}
-      <p>{todo.title}</p>
-      {/if}
-    </td>
-    <td><p>{todo.dueDate ? todo.dueDate : ''}<p></td>
-    <td><input type="button" on:click={() => {
-      update(markAsDone(todo));
-      updateTodos();
-      }}
-
-      value="Mark as done" /></td>
-  </tr>
-  {/each}
-</table>
+  <table>
+    <thead>
+      <tr>
+          <td><input bind:value={currentTodo.title} id="title" placeholder="Title" /></td>
+          <td><input bind:value={currentTodo.dueDate} type="date" id="date" /></td>
+          <td><input type="submit" value="Add" on:click={handleAdd} /></td>
+      </tr>
+    </thead>
+    <tr>
+      <td colspan="3">
+        <input bind:value={filter} on:change={updateTodos} id="filter" placeholder="Filter..." />
+      </td>
+    </tr>
+    {#each todos as todo}
+    <tr>
+      <td>
+        {#if todo.completedAt}
+        <p><s>{todo.title}</s></p>
+        {:else}
+        <p>{todo.title}</p>
+        {/if}
+      </td>
+      <td><p>{todo.dueDate ? todo.dueDate : ''}<p></td>
+      <td><input type="button" on:click={() => markAsDoneAndUpdate(todo)} value="Mark as done" /></td>
+    </tr>
+    {/each}
+  </table>
+</main>
--- src/todoList.js
+++ src/todoList.js
@@ -4,15 +4,21 @@ const matchingTodo = ({ title }) =>
   todos.find(todo => todo.title === title);
 
 export const add = todo => {
+  if (!todo || !todo.title || todo.title === "") {
+    throw new Error("title cannot be blank");
+  }
   if (matchingTodo(todo)) {
     throw new Error("that todo already exists");
   }
   todos = [ ...todos, todo ];
 };
 
-export const findMatching = title =>
+export const findAllMatching = title =>
   todos.filter(t => t.title.includes(title));
 
 export const update = updatedTodo => {
-  todos = [ ...todos.filter(todo => todo.title !== updatedTodo.title), updatedTodo ];
+  todos = [
+    ...todos.filter(todo => todo.title !== updatedTodo.title),
+    updatedTodo
+  ];
 };
