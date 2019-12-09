3953a8bbfa24eba1dc0c3b8183556f8aad11b16f
Daniel Irvine <daniel@danielirvine.com>
2019-12-09 19:57:33 +0000

    Split out test/tests.js into two files

--- test/TodoRepository.tests.js
+++ test/TodoRepository.tests.js
@@ -1,14 +1,6 @@
-import { emptyTodo, markAsDone } from "../src/todo.js";
+import { emptyTodo } from "../src/todo.js";
 import { TodoRepository } from "../src/todoRepository.js";
 import { beforeEach, describe, expect, it } from "concise-test";
-
-describe("todo", () => {
-  it("sets completedAt when calling markAsDone", () => {
-    const todo = emptyTodo();
-
-    expect(markAsDone(todo).completedAt).toBeDefined();
-  });
-});
 
 describe("TodoRepository", () => {
   const newTodo = { ...emptyTodo(), title: "test" };
