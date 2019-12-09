d2ec737b9a68008d5b8c16639231902a33b87ca2
Daniel Irvine <daniel@danielirvine.com>
2019-12-09 16:36:10 +0000

    Update tests to use new matchers

--- test/tests.js
+++ test/tests.js
@@ -1,13 +1,12 @@
 import { emptyTodo, markAsDone } from "../src/todo.js";
 import { TodoRepository } from "../src/todoRepository.js";
-import { beforeEach, describe, it } from "concise-test";
+import { beforeEach, describe, expect, it } from "concise-test";
 
 describe("todo", () => {
   it("sets completedAt when calling markAsDone", () => {
     const todo = emptyTodo();
 
-    if(!markAsDone(todo).completedAt)
-      throw new Error("completedAt not set when calling markAsDone");
+    expect(markAsDone(todo).completedAt).toBeDefined();
   });
 });
 
@@ -21,27 +20,15 @@ describe("TodoRepository", () => {
 
   describe("add", () => {
     it("throws an exception when adding a todo without a title", () => {
-      try {
-        repository.add(emptyTodo());
-        throw new Error("no error thrown when adding an empty todo");
-      } catch (e) {
-        if (e.message !== "title cannot be blank")
-          throw new Error("wrong message in guard clause when adding an empty todo");
-      }
+      expect(() => repository.add(emptyTodo()))
+        .toThrow(new Error("title cannot be blank"))
     });
 
     it("throws errors when adding a repeated todo", () => {
-      throw new Error('blah blah');
-
       repository.add(newTodo);
       const repeatedTodo = { ...newTodo };
-      try {
-        repository.add(repeatedTodo);
-        throw new Error("no error thrown when adding a repeated todo");
-      } catch (e) {
-        if (e.message !== "todo already exists")
-          throw new Error("wrong message in guard clause when adding an existing todo");
-      }
+      expect(() => repository.add(repeatedTodo))
+        .toThrow(new Error("todo already exists"));
     });
   });
 
@@ -51,14 +38,11 @@ describe("TodoRepository", () => {
     });
 
     it("finds an added todo", () => {
-      if (repository.findAllMatching('').length !== 1)
-        throw new Error("added todo was not returned");
+      expect(repository.findAllMatching('')).toHaveLength(1);
     });
 
     it("filters out todos that do not match filter", () => {
-      throw new Error('blah blah');
-      if (repository.findAllMatching('some other test').length !== 0)
-        throw new Error("filter was not applied when finding matches");
+      expect(repository.findAllMatching('some other test')).toHaveLength(0);
     });
   });
 });
