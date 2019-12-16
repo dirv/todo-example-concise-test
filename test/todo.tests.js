79cce1767b1545dad135b78b17414c9973a05ca2
Daniel Irvine <daniel@danielirvine.com>
2019-12-16 12:56:00 -0500

    Add shared, examples skipped tests and tags

--- test/todo.tests.js
+++ test/todo.tests.js
@@ -1,10 +1,17 @@
 import { emptyTodo, markAsDone } from "../src/todo.js";
 import { beforeEach, describe, expect, it } from "concise-test";
 
-describe("todo", () => {
-  it("sets completedAt when calling markAsDone", () => {
+describe("todo", { tags: [ "todo" ] }, () => {
+  it.skip("sets completedAt when calling markAsDone", () => {
     const todo = emptyTodo();
 
     expect(markAsDone(todo).completedAt).toBeDefined();
+  });
+});
+
+describe.skip('async tests', () => {
+  it('waits for a little bit', async () => {
+    // it.timesOutAfter(2000);
+     await new Promise((resolve, _) => setTimeout(resolve, 5000));
   });
 });
