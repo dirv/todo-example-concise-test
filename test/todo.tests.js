c0408f3e2923cd37a1fea32c93a1a2257f2ce61a
Daniel Irvine <daniel@danielirvine.com>
2022-12-15 16:47:56 +0000

    Add skipped tests

--- test/todo.tests.js
+++ test/todo.tests.js
@@ -7,9 +7,18 @@ import {
 } from "concise-test";
 
 describe("todo", () => {
-  it("sets completedAt when calling markAsDone", () => {
+  it.skip("sets completedAt when calling markAsDone", () => {
     const todo = emptyTodo();
 
     expect(markAsDone(todo).completedAt).toBeDefined();
+  });
+});
+
+describe.skip("async tests", () => {
+  it("waits for a little bit", async () => {
+    it.timesOutAfter(2000);
+    await new Promise((resolve, _) =>
+      setTimeout(resolve, 5000)
+    );
   });
 });
